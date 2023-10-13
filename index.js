7a2b4c037e2f7800b9f097b0561ac8cd5ca932e4
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-13 10:29:03 +0600

    Update User To Database

--- index.js
+++ index.js
@@ -34,6 +34,14 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.get('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const user = await userCollection.findOne(query);
+      res.send(user);
+    });
+
     // post
     app.post('/users', async (req, res) => {
       const user = req.body;
@@ -42,11 +50,24 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/users/:id', async (req, res) => {
+    app.put('/users/:id', async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const user = await userCollection.findOne(query);
-      res.send(user);
+      const user = req.body;
+      console.log(id, user);
+      const filter = { _id: new ObjectId(id) };
+      const option = { upsert: true };
+      const updatedUser = {
+        $set: {
+          name: user.name,
+          email: user.email,
+        },
+      };
+      const result = await userCollection.updateOne(
+        filter,
+        updatedUser,
+        option
+      );
+      res.send(result);
     });
 
     app.delete('/users/:id', async (req, res) => {
